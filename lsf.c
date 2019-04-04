c6ff0c141b0f1955eb3b7fc181f2c06e260ac8d3
CSE312Ubuntu <hcbayram@gtu.edu.tr>
2019-04-04 06:34:50 -0700

    ls utility redirection issues are not over yet

--- lsf.c
+++ lsf.c
@@ -1,13 +1,54 @@
 #include    <stdio.h>
 #include    <stdlib.h>
+#include    <string.h>
 #include    <dirent.h>
 #include	<sys/stat.h>
 #include    "lsf.h"
 
 
+
 int main(int argc, char **argv) {
-    DIR *curDir = opendir(".");
-    directoryOpenControl(curDir);
+    argumentNumberControl(argc);
+    DIR *curDir = NULL;
+    int pipeArr[2];
+    char *redirectFileName;
+    int isPipe;
+    printf("argc : %d\n", argc);
+    if (LSF_NO_OPTION(argc)) {
+        curDir = opendir(".");
+        redirectFileName = NULL;
+        isPipe = 0;
+    }
+    else if (LSF_REDIRECT(argc)) { // REDIRECT ATLANDI SU AN
+        printf("redirect\n");
+        if (!strcmp("m>", argv[1])) {
+        printf("redirect >\n");
+            curDir = opendir(".");
+            redirectFileName = (char *) malloc(sizeof(char) * 1024);
+            strcpy(redirectFileName, argv[2]);
+            isPipe = 0; 
+        }
+        else if (!strcmp("m<", argv[1])) {
+        printf("redirect <\n");
+            char *fileContent = NULL;
+            size_t len = 0;
+            FILE *fp = fopen(argv[2], "r");
+            if (!fp) {
+                fprintf(stderr, "File %s couldn't be found.\n", argv[2]);
+                exit(EXIT_FAILURE);
+            }
+            getline(&fileContent, &len, fp); // fileContent has a path for the directory whose files are displayed
+            redirectFileName = NULL;
+            isPipe = 0;
+            printf("\n\nfileContent : (%s)\n\n\n", fileContent);
+            curDir = opendir(fileContent);
+        }
+        else if (!strcmp("|", argv[1])) {
+
+        }
+
+    }
+    directoryOpenControl(curDir); // since curDir is not used so far and it will be used below, it can be checked here
     struct dirent *curFile = NULL;
     struct stat curFileStat;
 	while ((curFile=readdir(curDir)) != NULL) { // takes every file in the directory
@@ -16,9 +57,19 @@ int main(int argc, char **argv) {
 		if (curFile->d_type != DT_DIR) { // If the file is not a directory
             int statRetVal = stat(curFile->d_name, &curFileStat);
             statOpenControl(statRetVal, curFile->d_name);
-            printf("%s\t", curFile->d_name);
-            writeInfos(curFileStat);
+            writeInfos(curFile->d_name, curFileStat, pipeArr, redirectFileName, isPipe);
         }
+    }
+
+    free(redirectFileName);
+}
+
+
+
+void argumentNumberControl(int argc) {
+    if (argc > 3) {
+        fprintf(stderr, "More arguments than intended.\n");
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -32,6 +83,7 @@ void directoryOpenControl(const DIR* curDirPtr) {
 }
 
 
+
 void statOpenControl(int statRetVal, const char *name) {
     if (statRetVal < 0) {
         fprintf(stderr, "Stat of %s couldn't be opened for some reason.\n", name);
@@ -40,17 +92,44 @@ void statOpenControl(int statRetVal, const char *name) {
 }
 
 
-void writeInfos(const struct stat curFileStat) {
-    printf( (S_ISREG(curFileStat.st_mode)) ? "R" : "S");
-    printf( (curFileStat.st_mode & S_IRUSR) ? "r" : "-");
-    printf( (curFileStat.st_mode & S_IWUSR) ? "w" : "-");
-    printf( (curFileStat.st_mode & S_IXUSR) ? "x" : "-");
-    printf( (curFileStat.st_mode & S_IRGRP) ? "r" : "-");
-    printf( (curFileStat.st_mode & S_IWGRP) ? "w" : "-");
-    printf( (curFileStat.st_mode & S_IXGRP) ? "x" : "-");
-    printf( (curFileStat.st_mode & S_IROTH) ? "r" : "-");
-    printf( (curFileStat.st_mode & S_IWOTH) ? "w" : "-");
-    printf( (curFileStat.st_mode & S_IXOTH) ? "x" : "-");
-    printf("\t%ld",curFileStat.st_size);
-    printf("\n");
+
+void writeInfos(const char *fileName, const struct stat curFileStat, const int *pipeArr, const char *redirectFileName, int isPipe) {
+    if (redirectFileName) {
+        FILE *fp = fopen(redirectFileName, "w");
+        if (!fp) {
+            fprintf(stderr, "file %s couldn't be opened for some reason.\n", redirectFileName);
+            exit(EXIT_FAILURE);
+        }
+        else {
+            fprintf( fp, "%s\t", fileName);
+		    fprintf( fp, (S_ISREG(curFileStat.st_mode)) ? "R" : "S");
+		    fprintf( fp, (curFileStat.st_mode & S_IRUSR) ? "r" : "-");
+		    fprintf( fp, (curFileStat.st_mode & S_IWUSR) ? "w" : "-");
+		    fprintf( fp, (curFileStat.st_mode & S_IXUSR) ? "x" : "-");
+		    fprintf( fp, (curFileStat.st_mode & S_IRGRP) ? "r" : "-");
+		    fprintf( fp, (curFileStat.st_mode & S_IWGRP) ? "w" : "-");
+		    fprintf( fp, (curFileStat.st_mode & S_IXGRP) ? "x" : "-");
+		    fprintf( fp, (curFileStat.st_mode & S_IROTH) ? "r" : "-");
+		    fprintf( fp, (curFileStat.st_mode & S_IWOTH) ? "w" : "-");
+		    fprintf( fp, (curFileStat.st_mode & S_IXOTH) ? "x" : "-");
+		    fprintf( fp, "\t%ld\n",curFileStat.st_size);
+        }
+    }
+    else if (isPipe) {
+
+    }
+    else {
+        printf( "%s\t", fileName);
+	    printf( (S_ISREG(curFileStat.st_mode)) ? "R" : "S");
+	    printf( (curFileStat.st_mode & S_IRUSR) ? "r" : "-");
+	    printf( (curFileStat.st_mode & S_IWUSR) ? "w" : "-");
+	    printf( (curFileStat.st_mode & S_IXUSR) ? "x" : "-");
+	    printf( (curFileStat.st_mode & S_IRGRP) ? "r" : "-");
+	    printf( (curFileStat.st_mode & S_IWGRP) ? "w" : "-");
+	    printf( (curFileStat.st_mode & S_IXGRP) ? "x" : "-");
+	    printf( (curFileStat.st_mode & S_IROTH) ? "r" : "-");
+	    printf( (curFileStat.st_mode & S_IWOTH) ? "w" : "-");
+	    printf( (curFileStat.st_mode & S_IXOTH) ? "x" : "-");
+	    printf("\t%ld\n",curFileStat.st_size);
+    }
 }
--- lsf.h
+++ lsf.h
@@ -3,10 +3,21 @@
 
 #include    <dirent.h>
 
-void writeInfos(const struct stat curFileStat);
 
+/*
+Parameters
+fileName : name of file whose infos are written into
+curFileStat : struct of the file that keeps its infos
+pipeArr : addresses of pipe, if it is written into a pipe
+redirectFileName : name of the file where I/O is redirected to
+*/
+void writeInfos(const char *fileName, const struct stat curFileStat, const int *pipeArr, const char *redirectFileName, int isPipe);
 
 void directoryOpenControl(const DIR* curDirPtr);
 void statOpenControl(int statRetVal, const char *name);
+void argumentNumberControl(int argc);
+
+#define LSF_NO_OPTION(a) ((a == 1) ? (1) : (0))
+#define LSF_REDIRECT(a)  ((a == 3) ? (1) : (0))
 
 #endif
