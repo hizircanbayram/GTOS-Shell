3822969f66718c0ac4b548f7f58a1e5b3ee513d6
CSE312Ubuntu <hcbayram@gtu.edu.tr>
2019-04-05 01:45:19 -0700

    Cat is done, pipe is missing

--- cat.c
+++ cat.c
@@ -1,28 +1,39 @@
-#include    <stdio.h>
-#include    <stdlib.h>
 #include    "cat.h"
 
 
 
 
 int main(int argc, char **argv) {
+    int pipeArr[2];
     if (CAT_ONE_ARG(argc)) {
-        writeTo(argv[1], ONE_ARGUMENT); 
+        writeTo(argv[1], ONE_ARGUMENT, pipeArr, NULL); 
     }
     else if (CAT_REDIRECT(argc)) {
-
+        if (!strcmp(">", argv[2])) {
+            writeTo(argv[1], OUTPUT_REDIRECT, pipeArr, argv[3]);
+        }
+        else if (!strcmp("<", argv[1])) {
+            writeTo(NULL, INPUT_REDIRECT, pipeArr, argv[2]);
+        }
+        else if (!strcmp("|", argv[2])) {
+            writeTo(argv[1], PIPE_REDIRECT, pipeArr, argv[3]);
+        }
+        else {
+            fprintf(stderr, "CAT | Undefined operation.\n");
+            exit(EXIT_FAILURE);
+        }
+        
     }    
     else {
-        fprintf(stderr, "Invalud number of arguments are supplied.\n");
+        fprintf(stderr, "CAT | Invalid number of arguments are supplied.\n");
     }
-    
     
     return 0;
 }
 
 
 
-void writeTo(const char *fileName, int flag) {
+void writeTo(const char *fileName, int flag, const int *pipeArr, const char *redirectFileName) {
     FILE *fp = fopen(fileName, "r");
     if (!fp && fileName) {
         fprintf(stderr, "File %s couldn't be opened for some reason.\n", fileName);
@@ -37,12 +48,34 @@ void writeTo(const char *fileName, int flag) {
             free(line);        
             line = NULL;
         }
-       
     }
     else if (INPUT_REDIRECT == flag) {
-
+        char *line = NULL;
+        size_t len = 0;
+        FILE *redirect_fp = fopen(redirectFileName, "r");
+        if (!redirect_fp) {
+            fprintf(stderr, "CAT | File %s couldn't be opened for input redirecting.\n", redirectFileName);
+            exit(EXIT_FAILURE);
+        } 
+        while (getline(&line, &len, redirect_fp) != -1) {
+            printf("%s", line);
+            free(line);        
+            line = NULL;
+        }
     }
     else if (OUTPUT_REDIRECT == flag) {
+        char *line = NULL;
+        size_t len = 0;
+        FILE *redirect_fp = fopen(redirectFileName, "w");
+        if (!redirect_fp) {
+            fprintf(stderr, "CAT | File %s couldn't be opened for output redirecting.\n", redirectFileName);
+            exit(EXIT_FAILURE);
+        } 
+        while (getline(&line, &len, fp) != -1) {
+            fprintf(redirect_fp, "%s", line);
+            free(line);        
+            line = NULL;
+        }
 
     } 
     else if (PIPE_REDIRECT == flag) {
--- cat.h
+++ cat.h
@@ -1,8 +1,12 @@
 #ifndef CAT_H_HCB
 #define CAT_H_HCB
 
+#include    <stdio.h>
+#include    <stdlib.h>
+#include    <string.h>
+
 #define CAT_ONE_ARG(argc) ((argc == 2) ? 1 : 0)
-#define CAT_REDIRECT(argc) ((argc == 3) ? 1 : 0)
+#define CAT_REDIRECT(argc) (((argc == 3) || (argc == 4)) ? 1 : 0)
 
 #define ONE_ARGUMENT 1
 #define INPUT_REDIRECT 2
@@ -13,6 +17,6 @@
 PARAMETERS
 fileName : name of the file whose content is written into
 flag : indicate either of the operations : one argument, redirect types or pipe*/
-void writeTo(const char *fileName, int flag);
+void writeTo(const char *fileName, int flag, const int *pipeArr, const char *redirectFileName);
 
 #endif
